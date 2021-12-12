227ade2758349128bb5a66b9b3b7fb8214a2ce7f
gustavovs88 <gustavovs88@gmail.com>
2021-12-12 15:56:46 -0300

    Almost finished

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 const introAnimated = document.getElementById("introAnimated");
 const ctx = introAnimated.getContext("2d");
-let canvasWidth = introAnimated.width = 700;
-let canvasHeight = introAnimated.height = 400;
+const screenWidth = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+let canvasWidth = introAnimated.width = screenWidth < 720 ? 320 : 700;
+let canvasHeight = introAnimated.height = screenWidth < 720 ? 280 : 400;
 let particleArray = [];
 let mappedImage = [];
-let fontSize = 16
-let fontSizeFactor = 5;
-let positionAdjustX = 10;
-let positionAdjustY = 15;
+let fontSize = screenWidth < 720 ? 14 : 16
+let fontSizeFactor = screenWidth < 720 ? 3 : 5;
+let positionAdjustX = screenWidth < 720 ? 5 : 10;
+let positionAdjustY = screenWidth < 720 ? 10 : 15;
 // Change it to the text you want to use
-let text = "Hey!\nI'm Gustavo,\nweb developer.";
+let text = "Hey!\nI'm Gustavo,\nWeb Developer.";
 //Declare mouse radius and position
 const mouse = {
   x: null,
   y: null,
-  radius: 75,
+  radius: screenWidth < 720 ? 30 : 75,
 };
 
 //Listen to mouse movement
@@ -32,7 +33,7 @@ var lines = text.split('\n');
 for (var i = 0; i<lines.length; i++){
     ctx.fillText(lines[i], positionAdjustX, positionAdjustY + (i*lineheight));
 
-}
+};
 
 
 //Read the text source and map it into mappedImage array
@@ -42,7 +43,7 @@ function calculateRelativeBrightness(red, green, blue) {
     Math.sqrt(red * red * 0.299 + green * green * 0.587 + blue * blue * 0.114) /
     100
   );
-}
+};
 for (let y = 0; y < introAnimated.height; y++) {
   let row = [];
   for (let x = 0; x < introAnimated.width; x++) {
@@ -55,9 +56,9 @@ for (let y = 0; y < introAnimated.height; y++) {
     const cellColorCalc = "rgb(" + red + "," + green + "," + blue + ")";
     const cell = [(cellBrightness = brightness), (cellColor = cellColorCalc)];
     row.push(cell);
-  }
+  };
   mappedImage.push(row);
-}
+};
 // Defining the particle and mesh constructor and methods
 class Particle {
   constructor(x, y) {
@@ -65,7 +66,9 @@ class Particle {
     this.y = y;
     this.position1 = Math.floor(this.y) / fontSizeFactor - positionAdjustY;
     this.position2 = Math.floor(this.x) / fontSizeFactor - positionAdjustX;
-    this.size = Math.random() * 3 + 1;
+    this.size = screenWidth < 720 ? 3 : 1;;
+
+    // this.size = screenWidth < 720 ? Math.random() * 1.5 + 1 : Math.random() * 3 + 1;
     this.baseX = this.x;
     this.baseY = this.y;
     this.density = Math.random() * 30 + 1;
@@ -74,7 +77,7 @@ class Particle {
     this.color = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(
       Math.random() * 255
     )}, ${Math.floor(Math.random() * 255)}, 1`
-  }
+  };
   draw() {
     ctx.fillStyle = this.color;
     ctx.beginPath();
@@ -100,27 +103,14 @@ class Particle {
       if (this.x !== this.baseX) {
         let dx = this.x - this.baseX;
         this.x -= dx / 10;
-      }
+      };
       if (this.y !== this.baseY) {
         let dy = this.y - this.baseY;
         this.y -= dy / 10;
-      }
-      // Uncoment below if you want the particles to disappear after mouse has passed
-      // if (
-      //   this.mouseHasPassed === true
-      //    &&
-      //   Math.floor(this.baseX) === Math.floor(this.x)
-      // ) {
-      //   this.color = `rgb(${Math.floor(Math.random() * 255)},${Math.floor(
-      //     Math.random() * 255
-      //   )},${Math.floor(
-      //     Math.random() * 255
-          
-      //   )},${(this.particleTransparency -= 0.1)})`;
-      // }
-    }
-  }
-}
+      };
+    };
+  };
+};
 function init() {
   for (let y = 0, y2 = textCoordinates.height; y < y2; y++) {
     for (let x = 0, x2 = textCoordinates.width; x < x2; x++) {
@@ -132,10 +122,10 @@ function init() {
         particleArray.push(
           new Particle(positionX * fontSizeFactor, positionY * fontSizeFactor)
         );
-      }
-    }
-  }
-}
+      };
+    };
+  };
+};
 
 init();
 
@@ -152,7 +142,7 @@ function animate() {
 }
 animate();
 ctx.lineWidth = Math.random() * 2;
-
+var connectDistance = screenWidth < 720 ? 4 : 10
 function connect() {
   let opacityValue = 1;
   for (let a = 0; a < particleArray.length; a++) {
@@ -161,7 +151,7 @@ function connect() {
       let dy = particleArray[a].y - particleArray[b].y;
       let distance = Math.sqrt(dx * dx + dy * dy);
       if (
-        distance < 10
+        distance < connectDistance
       ) {
         opacityValue = 0.8 - distance / 20;
         ctx.strokeStyle = `rgb(255,255,255, +${opacityValue})`
@@ -169,10 +159,52 @@ function connect() {
         ctx.moveTo(particleArray[a].x, particleArray[a].y);
         ctx.lineTo(particleArray[b].x, particleArray[b].y);
         ctx.stroke();
-      }
-    }
-  }
-}
+      };
+    };
+  };
+};
 
-var projectsCarrousel = document.querySelector('#projectsCarrousel')
-var carousel = new bootstrap.Carousel(projectsCarrousel)
+
+const frontendDescription = document.getElementById('frontendDescription');
+const backendDescription = document.getElementById('backendDescription');
+const flipCard = document.querySelector('.flip-card')
+flipCard.addEventListener("mouseover", function() {
+  frontendDescription.classList.toggle('hide')
+  backendDescription.classList.toggle('hide')
+
+})
+flipCard.addEventListener("mouseout", function() {
+  frontendDescription.classList.toggle('hide')
+  backendDescription.classList.toggle('hide')
+
+})
+
+const myPhoto = document.querySelector('.my-photo')
+
+const observeMyPhoto = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      myPhoto.classList.add('move-to-right');
+      return;
+    }
+
+    myPhoto.classList.remove('move-to-right');
+  });
+});
+
+observeMyPhoto.observe(document.querySelector('.my-photo'));
+
+
+const footer = document.querySelector('footer')
+const footerAnimation = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      footer.classList.toggle('hide');
+      return;
+    }
+
+    footer.classList.remove('hide');
+  });
+});
+
+footerAnimation.observe(document.querySelector('#header'));
